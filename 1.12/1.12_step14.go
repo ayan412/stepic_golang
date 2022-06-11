857a7bf7c63b11c573a90b239ebdc65b50bb7115
ayan412 <mussatayev.ayan@gmail.com>
2022-06-11 18:17:37 +0600

    added to git

---
+++ 1.12/1.12_step14.go
@@ -0,0 +1,40 @@
+/*
+На ввод подаются пять целых чисел, которые записываются в массив. Однако эта часть программы уже написана.
+
+Вам нужно написать фрагмент кода, с помощью которого можно найти и вывести максимальное число в этом массиве.
+
+Sample Input:
+
+2
+3
+56
+45
+21
+Sample Output:
+
+56
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	array := [5]int{}
+	var a int
+	for i := 0; i < 5; i++ {
+		fmt.Scan(&a)
+		array[i] = a
+	}
+	// здесь ваш код
+	// ...
+	//fmt.Println(array)
+	var mxn int = array[0]
+	for i := 0; i < 5; i++ {
+		if array[i] > mxn {
+			mxn = array[i]
+		}
+	}
+
+	fmt.Println(mxn)
+}

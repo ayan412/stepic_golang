2e99f8622ee630b1bf253a5f1eb88ebb5a2344b8
ayan412 <mussatayev.ayan@gmail.com>
2022-06-22 20:01:59 +0600

    1.13_step11.go

---
+++ 1.13/1.13_step11.go
@@ -0,0 +1,41 @@
+/*
+Самое большое число, кратное 7
+Найдите самое большее число на отрезке от a до b, кратное 7 .
+
+Входные данные
+Вводится два целых числа a и b (a≤b).
+
+Выходные данные
+Найдите самое большее число на отрезке от a до b (отрезок включает в себя числа a и b), кратное 7 , или выведите "NO" - если таковых нет.
+
+Sample Input:
+
+100
+500
+Sample Output:
+
+497
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	var slc []int
+	for i := b; i >= a; i-- {
+		if i%7 == 0 {
+			//fmt.Println(i)
+			slc = append(slc, i)
+		}
+	}
+	//fmt.Println(len(slc))
+	if len(slc) == 0 {
+		fmt.Println("NO")
+	} else {
+		fmt.Println(slc[0])
+	}
+
+}

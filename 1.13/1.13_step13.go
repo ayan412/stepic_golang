b337907cdf860339e5fe7528e3da9cfcc6668356
ayan412 <mussatayev.ayan@gmail.com>
2022-06-27 22:06:07 +0600

    added new solution

--- 1.13/1.13_step13.go
+++ 1.13/1.13_step13.go
@@ -43,3 +43,18 @@ func main() {
 	}
 
 }
+
+// Как лаконично решали другие
+/*
+
+package main
+import . "fmt"
+func main(){
+    var n int
+    Scan(&n)
+    for d:=1;d<=n;d<<=1{
+        Print(d," ")
+    }
+}
+
+*/
